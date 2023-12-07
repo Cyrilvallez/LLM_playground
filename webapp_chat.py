0c5c8172c37b456fc57daafb4be7f45bca5e8934
Cyril Vallez <cyril.vallez@gmail.com>
2023-12-07 16:42:20 +0100

    Use new version of textwiz with gradio functions

--- webapp_chat.py
+++ webapp_chat.py
@@ -10,6 +10,7 @@ import gradio as gr
 
 from textwiz import HFModel, TextContinuationStreamer
 from textwiz.conversation_template import GenericConversation, CONVERSATION_MAPPING
+import textwiz.web_interface as wi
 from helpers import utils
 
 # Default model to load at start-up
@@ -31,284 +32,29 @@ LOGGERS = {}
 def chat_generation(conversation: GenericConversation, prompt: str, max_new_tokens: int, do_sample: bool,
                     top_k: int, top_p: float, temperature: float, use_seed: bool,
                     seed: int) -> tuple[str, GenericConversation, list[list]]:
-    """Chat generation.
-
-    Parameters
-    ----------
-    conversation : GenericConversation
-        Current conversation. This is the value inside a gr.State instance.
-    prompt : str
-        The prompt to the model.
-    max_new_tokens : int
-        How many new tokens to generate.
-    do_sample : bool
-        Whether to introduce randomness in the generation.
-    top_k : int
-        How many tokens with max probability to consider for randomness.
-    top_p : float
-        The probability density covering the new tokens to consider for randomness.
-    temperature : float
-        How to cool down the probability distribution. Value between 1 (no cooldown) and 0 (greedy search,
-        no randomness).
-    use_seed : bool, optional
-        Whether to use a fixed seed for reproducibility., by default False.
-    seed : int
-        An optional seed to force the generation to be reproducible.
-
-    Returns
-    -------
-    tuple[str, GenericConversation, list[list]]
-        Components (prompt, conversation, output)
-    """
-    
-    if not use_seed:
-        seed = None
-
-    timeout = 20
-
-    # To show text as it is being generated
-    streamer = TextIteratorStreamer(MODEL.tokenizer, skip_prompt=True, timeout=timeout, skip_special_tokens=True)
-
-    conv_copy = copy.deepcopy(conversation)
-    conv_copy.append_user_message(prompt)
-    
-    # We need to launch a new thread to get text from the streamer in real-time as it is being generated. We
-    # use an executor because it makes it easier to catch possible exceptions
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        # This will update `conversation` in-place
-        future = executor.submit(MODEL.generate_conversation, prompt, system_prompt=None, conv_history=conversation,
-                                 max_new_tokens=max_new_tokens, do_sample=do_sample, top_k=top_k, top_p=top_p,
-                                 temperature=temperature, seed=seed, truncate_if_conv_too_long=True, streamer=streamer)
-        
-        # Get results from the streamer and yield it
-        try:
-            generated_text = ''
-            for new_text in streamer:
-                generated_text += new_text
-                # Update model answer (on a copy of the conversation) as it is being generated
-                conv_copy.model_history_text[-1] = generated_text
-                # The first output is an empty string to clear the input box, the second is the format output
-                # to use in a gradio chatbot component
-                yield '', conv_copy, conv_copy.to_gradio_format()
-
-        # If for some reason the queue (from the streamer) is still empty after timeout, we probably
-        # encountered an exception
-        except queue.Empty:
-            e = future.exception()
-            if e is not None:
-                raise gr.Error(f'The following error happened during generation: {repr(e)}')
-            else:
-                raise gr.Error(f'Generation timed out (no new tokens were generated after {timeout} s)')
-    
-    # Update the chatbot with the real conversation (which may be slightly different due to postprocessing)
-    yield '', conversation, conversation.to_gradio_format()
-
+    yield from wi.chat_generation(MODEL, conversation=conversation, prompt=prompt, max_new_tokens=max_new_tokens,
+                                  do_sample=do_sample, top_k=top_k, top_p=top_p, temperature=temperature, use_seed=use_seed,
+                                  seed=seed)
 
 
 def continue_generation(conversation: GenericConversation, additional_max_new_tokens: int, do_sample: bool,
                         top_k: int, top_p: float, temperature: float, use_seed: bool,
                         seed: int) -> tuple[GenericConversation, list[list]]:
-    """Continue the last turn of the model output.
-
-    Parameters
-    ----------
-    conversation : GenericConversation
-        Current conversation. This is the value inside a gr.State instance.
-    additional_max_new_tokens : int
-        How many new tokens to generate.
-    do_sample : bool
-        Whether to introduce randomness in the generation.
-    top_k : int
-        How many tokens with max probability to consider for randomness.
-    top_p : float
-        The probability density covering the new tokens to consider for randomness.
-    temperature : float
-        How to cool down the probability distribution. Value between 1 (no cooldown) and 0 (greedy search,
-        no randomness).
-    use_seed : bool
-        Whether to use a fixed seed for reproducibility.
-    seed : int
-        An optional seed to force the generation to be reproducible.
-
-    Returns
-    -------
-    tuple[str, list[list]
-        Components (conversation, output).
-    """
-
-    if len(conversation) == 0:
-        gr.Warning(f'You cannot continue an empty conversation.')
-        yield conversation, conversation.to_gradio_format()
-        return
-    if conversation.model_history_text[-1] is None:
-        gr.Warning('You cannot continue an empty last turn.')
-        yield conversation, conversation.to_gradio_format()
-        return
-
-    if not use_seed:
-        seed = None
-
-    timeout = 20
-
-    # To show text as it is being generated
-    streamer = TextContinuationStreamer(MODEL.tokenizer, skip_prompt=True, timeout=timeout, skip_special_tokens=True)
-
-    conv_copy = copy.deepcopy(conversation)
-    
-    # We need to launch a new thread to get text from the streamer in real-time as it is being generated. We
-    # use an executor because it makes it easier to catch possible exceptions
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        # This will update `conversation` in-place
-        future = executor.submit(MODEL.continue_last_conversation_turn, conv_history=conversation,
-                                 max_new_tokens=additional_max_new_tokens, do_sample=do_sample, top_k=top_k, top_p=top_p,
-                                 temperature=temperature, seed=seed, truncate_if_conv_too_long=True, streamer=streamer)
-        
-        # Get results from the streamer and yield it
-        try:
-            generated_text = conv_copy.model_history_text[-1]
-            for new_text in streamer:
-                generated_text += new_text
-                # Update model answer (on a copy of the conversation) as it is being generated
-                conv_copy.model_history_text[-1] = generated_text
-                # The first output is an empty string to clear the input box, the second is the format output
-                # to use in a gradio chatbot component
-                yield conv_copy, conv_copy.to_gradio_format()
-
-        # If for some reason the queue (from the streamer) is still empty after timeout, we probably
-        # encountered an exception
-        except queue.Empty:
-            e = future.exception()
-            if e is not None:
-                raise gr.Error(f'The following error happened during generation: {repr(e)}')
-            else:
-                raise gr.Error(f'Generation timed out (no new tokens were generated after {timeout} s)')
-
-    # Update the chatbot with the real conversation (which may be slightly different due to postprocessing)
-    yield conversation, conversation.to_gradio_format()
-
+    yield from wi.continue_generation(MODEL, conversation=conversation, additional_max_new_tokens=additional_max_new_tokens,
+                                      do_sample=do_sample, top_k=top_k, top_p=top_p, temperature=temperature,
+                                      use_seed=use_seed, seed=seed)
 
 
 def retry_chat_generation(conversation: GenericConversation, max_new_tokens: int, do_sample: bool,
                           top_k: int, top_p: float, temperature: float, use_seed: bool,
                           seed: int) -> tuple[GenericConversation, list[list]]:
-    """Chat generation.
-
-    Parameters
-    ----------
-    conversation : GenericConversation
-        Current conversation. This is the value inside a gr.State instance.
-    max_new_tokens : int
-        How many new tokens to generate.
-    do_sample : bool
-        Whether to introduce randomness in the generation.
-    top_k : int
-        How many tokens with max probability to consider for randomness.
-    top_p : float
-        The probability density covering the new tokens to consider for randomness.
-    temperature : float
-        How to cool down the probability distribution. Value between 1 (no cooldown) and 0 (greedy search,
-        no randomness).
-    use_seed : bool
-        Whether to use a fixed seed for reproducibility.
-    seed : int
-        An optional seed to force the generation to be reproducible.
-
-    Returns
-    -------
-    tuple[GenericConversation, list[list]]
-        Components (conversation, output)
-    """
-
-    if len(conversation) == 0:
-        gr.Warning(f'You cannot retry generation on an empty conversation.')
-        yield conversation, conversation.to_gradio_format()
-        return
-    if conversation.model_history_text[-1] is None:
-        gr.Warning('You cannot retry generation on an empty last turn')
-        yield conversation, conversation.to_gradio_format()
-        return
-    
-    if not use_seed:
-        seed = None
-
-    timeout = 20
-
-    # Remove last turn
-    prompt = conversation.user_history_text[-1]
-    _ = conversation.user_history_text.pop(-1)
-    _ = conversation.model_history_text.pop(-1)
-
-    # To show text as it is being generated
-    streamer = TextIteratorStreamer(MODEL.tokenizer, skip_prompt=True, timeout=timeout, skip_special_tokens=True)
-
-    conv_copy = copy.deepcopy(conversation)
-    conv_copy.append_user_message(prompt)
-    
-    # We need to launch a new thread to get text from the streamer in real-time as it is being generated. We
-    # use an executor because it makes it easier to catch possible exceptions
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        # This will update `conversation` in-place
-        future = executor.submit(MODEL.generate_conversation, prompt, system_prompt=None, conv_history=conversation,
-                                 max_new_tokens=max_new_tokens, do_sample=do_sample, top_k=top_k, top_p=top_p,
-                                 temperature=temperature, seed=seed, truncate_if_conv_too_long=True, streamer=streamer)
-        
-        # Get results from the streamer and yield it
-        try:
-            generated_text = ''
-            for new_text in streamer:
-                generated_text += new_text
-                # Update model answer (on a copy of the conversation) as it is being generated
-                conv_copy.model_history_text[-1] = generated_text
-                # The first output is an empty string to clear the input box, the second is the format output
-                # to use in a gradio chatbot component
-                yield conv_copy, conv_copy.to_gradio_format()
-
-        # If for some reason the queue (from the streamer) is still empty after timeout, we probably
-        # encountered an exception
-        except queue.Empty:
-            e = future.exception()
-            if e is not None:
-                raise gr.Error(f'The following error happened during generation: {repr(e)}')
-            else:
-                raise gr.Error(f'Generation timed out (no new tokens were generated after {timeout} s)')
-    
-    
-    # Update the chatbot with the real conversation (which may be slightly different due to postprocessing)
-    yield conversation, conversation.to_gradio_format()
-
+    yield from wi.retry_chat_generation(MODEL, conversation=conversation, max_new_tokens=max_new_tokens,
+                                        do_sample=do_sample, top_k=top_k, top_p=top_p, temperature=temperature,
+                                        use_seed=use_seed, seed=seed)
 
 
 def authentication(username: str, password: str) -> bool:
-    """Simple authentication method.
-
-    Parameters
-    ----------
-    username : str
-        The username provided.
-    password : str
-        The password provided.
-
-    Returns
-    -------
-    bool
-        Return True if both the username and password match some credentials stored in `CREDENTIALS_FILE`. 
-        False otherwise.
-    """
-
-    with open(CREDENTIALS_FILE, 'r') as file:
-        # Read lines and remove whitespaces
-        lines = [line.strip() for line in file.readlines() if line.strip() != '']
-
-    valid_usernames = lines[0::2]
-    valid_passwords = lines[1::2]
-
-    if username in valid_usernames:
-        index = valid_usernames.index(username)
-        # Check that the password also matches at the corresponding index
-        if password == valid_passwords[index]:
-            return True
-    
-    return False
+    return wi.simple_authentication(CREDENTIALS_FILE, username, password)
     
 
 def clear_chatbot(username: str) -> tuple[GenericConversation, str, list[list]]:
