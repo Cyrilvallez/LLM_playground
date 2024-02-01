f57220e6a850974b21875c2366bbb34e18bdfc90
Cyril Vallez <cyril.vallez@gmail.com>
2024-02-01 15:37:13 +0100

    test

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh" -O ~/miniforge.sh
 bash ~/miniforge.sh -b -p ~/miniforge3
 
+
 # Activate base env and run init for the future
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate
