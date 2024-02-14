d450dde9f6650acfc968c4394d66558e71c4b660
Cyril Vallez <cyril.vallez@gmail.com>
2024-02-14 11:57:24 +0100

    Update webapp_chat.sh

--- webapp_chat.sh
+++ webapp_chat.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=60G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:v100:2
 #SBATCH --chdir=/cluster/raid/home/vacy/LLM_playground
 
 # Initialize the shell to use local conda
