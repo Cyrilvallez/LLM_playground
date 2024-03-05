0a0f1869fa6a475ecf2587f4a3bdf4d01d618db2
Cyril Vallez <cyril.vallez@gmail.com>
2024-03-05 18:43:24 +0100

    Update webapp.sh

--- webapp.sh
+++ webapp.sh
@@ -16,7 +16,7 @@ eval "$(conda shell.bash hook)"
 # Activate (local) env
 conda activate llm-playground
 
-../frp_server/frp_0.54.0_linux_amd64/frpc -c ../frp_server/frpc/frpc_play1.toml &
+../frp_server/frp_0.54.0_linux_amd64/frpc -c ../frp_server/frpc/frpc_play.toml &
 python3 -u webapp.py "$@"
 
 conda deactivate
